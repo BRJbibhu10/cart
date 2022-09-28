be504e3959f28676f2d2b4749af62358addd2ad6
bibhu <bb@bb.com>
2022-09-28 14:05:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  const name = 'Bibhu';
+  const isLoggedIn = true ;
+
+  return (
+    <div className="App">
+      <h1>Cart</h1>
+    </div>
+  );
+}
+
+export default App;
